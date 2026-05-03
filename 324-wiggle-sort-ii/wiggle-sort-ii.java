0288dc058ba1d935e63537b69d4bedb16a7ebcff
kanishkupadhyay1 <kanishk.upadhyay2006@gmail.com>
2026-05-03 14:26:43 +0530

    Time: 9 ms (62.61%) | Memory: 48.8 MB (60.82%) - LeetSync

---
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        int mid = (n - 1) / 2;
+        int end = n - 1;
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                nums[i] = sorted[mid--];   
+            } else {
+                nums[i] = sorted[end--];   
+            }
+        }
+    }
+}
